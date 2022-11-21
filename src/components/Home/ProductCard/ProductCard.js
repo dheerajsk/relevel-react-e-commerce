e837062d7e27831146adc4d41bed98dca2327e83
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-11-21 21:22:16 +0530

    Products card

--- src/components/Home/ProductCard/ProductCard.js
+++ src/components/Home/ProductCard/ProductCard.js
@@ -1,17 +1,35 @@
 import "./ProductCard.css";
 
 function ProductCard(values) {
+  let stars = [];
+  for (let i = 0; i < Number(values.item.rating.rate); i++) {
+    stars[i] = 1;
+  }
 
+  console.log(stars);
   return (
     <div className="card">
-      <img
-        src={values.item.image}
-        alt="..."></img>
+      <h5 className="card-title">{values.item.title}</h5>
+      <img src={values.item.image} alt="..."></img>
       <hr />
       <div className="card-body">
-        <h5 className="card-title">{values.item.name}</h5>
-        <p className="card-text">&#8377; {values.item.price}</p>
-        <p className="card-text">{values.item.detail}</p>
+        <p className="card-text">{values.item.category}</p>
+        <p className="stars">
+          {stars.map((x) => (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-star-fill"
+              viewBox="0 0 16 16">
+              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+            </svg>
+          ))}{" "}
+          &nbsp;&nbsp;
+          <span>{values.item.rating.count}</span>
+        </p>
+        <p className="card-text">{values.item.description}</p>
         <a href="#" className="btn btn-success flex-box">
           <svg
             xmlns="http://www.w3.org/2000/svg"
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
+
 import ProductCard from "../../components/Home/ProductCard/ProductCard";
+
 import Footer from "../../components/Shared/Footer/Footer";
 import Header from "../../components/Shared/Header/Header";
 
@@ -8,33 +10,39 @@ function Home() {
   // whenever there is achange in products, it will force component refresh.
 
   const [products, setProducts] = useState([]);
-  // const [click, setClick] = useState(0);
+  const [click, setClick] = useState(0);
   // const [click2, setClick2] = useState(100);
   // let data = "My Data";
 
   useEffect(() => {
-    fetch("Product.json")
+    fetch("https://fakestoreapi.com/products")
       // calling json function.
       .then((res) => res.json())
       // listening for json function to return.
       .then((res) => {
+        console.log("fetching data");
+        res.forEach(o=>{
+          o.rating.rate=Math.ceil(Number(o.rating.rate));
+        })
+        console.log(res);
         setProducts(res);
       });
-  }, []);
+  }, [click]);
 
   return (
     <div>
       <Header />
       {/* Products */}
-      <div>
-        {/* <h1>{click}</h1>
-        <h1>{click2}</h1>
-        <button
+      <button
           onClick={() => {
             setClick(click + 1);
           }}>
           Click
         </button>
+      <div>
+        {/* <h1>{click}</h1>
+        <h1>{click2}</h1>
+       
         <button
           onClick={() => {
             setClick2(click2 + 1);
