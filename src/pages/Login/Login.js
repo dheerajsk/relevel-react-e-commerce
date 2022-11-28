4b6f0b9ea2579544ad5141fea11aabaf813c2fac
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-11-28 20:08:51 +0530

    css fix

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,7 +4,7 @@ function Login() {
   function handleLogin() {}
 
   return (
-    <div className="container">
+    <div className="login-container">
       <h3>Please login.</h3>
       <form onSubmit={handleLogin} className="login-form" action="">
         <div className="mb-3">
