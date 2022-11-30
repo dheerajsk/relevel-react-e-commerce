409f15de3cd0bc1535aec4a63ede7e539ca9a041
coderAsh1 <ashutoshsasmal1@gmail.com>
2022-11-30 18:46:46 +0530

    reduce fixed

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -29,9 +29,10 @@ function Cart() {
   // 1. Update Total Price = sum of price*quantity for each cart items.
   function updateTotalPrice(res) {
     let sum = 0;
-    for (let i = 0; i < res.length; i++) {
-      sum = sum + Number(res[i].price) * Number(res[i].qty);
-    }
+
+    sum = res.reduce((prev, curr) => {
+      return prev + +curr.price;
+    }, 0);
     setTotalPrice(sum);
   }
 
