6de5d9846b0577c163c5b87efaded17ce24b5d57
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-12-03 12:52:43 +0530

    Context API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+
+import { createContext } from 'react';
+
+import Register from './pages/Register/Register';
+import Login from './pages/Login/Login';
+import Cart from './pages/Cart/Cart';
+import { useState } from 'react';
+
 function App() {
+
+  // 1. Create Context for application.
+  const AppContext = createContext();
+
+  // 2. Initialize Store.
+  const [cartItems, setCartItems]=useState([]);
+
+  // 3. Dispatchers.
+  const dispatcherEvents = (actionType, payload)=>{
+      switch(actionType){
+        case "ADD_ITEM":{
+          setCartItems(...cartItems, payload);
+          break;
+        }
+        default:{
+          console.log("INVALID...");
+        }
+      }
+  }
+
+
   return (
-    <div>
-    <h2>E-Commerce Application... Coming Soon...</h2>
-    <input type="text" ></input>
-    </div>
+    // 3. Initializing Context.
+    <AppContext.Provider value={{cartItems, dispatcherEvents}}>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='register' element={<Register />}></Route>
+      <Route path='login' element={<Login />}></Route>
+      <Route path='cart' element={<Cart />}></Route>
+    </Routes>
+  </BrowserRouter>
+  </AppContext.Provider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,25 +6,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap';
 
 import reportWebVitals from './reportWebVitals';
-import Register from './pages/Register/Register';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login/Login';
-import Cart from './pages/Cart/Cart';
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='register' element={<Register />}></Route>
-      <Route path='login' element={<Login />}></Route>
-      <Route path='cart' element={<Cart />}></Route>
-    </Routes>
-  </BrowserRouter>
-  // <Home />
-  // <Register />
-  // </React.StrictMode>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
