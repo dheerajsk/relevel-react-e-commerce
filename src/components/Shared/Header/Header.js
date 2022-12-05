4d2dbb7e1fce7f3d5f219c142d7e7c1da12cb503
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-12-05 22:10:18 +0530

    Cart page update

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,6 +1,7 @@
 import { useContext} from 'react';
 import './Header.css';
 import AppContext from '../../../context';
+import { Link } from 'react-router-dom';
 
 function Header(props){
 
@@ -13,20 +14,20 @@ function Header(props){
   <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z"/>
 </svg>
         &nbsp;&nbsp;
-          <a className="navbar-brand" href="/">E-Shop</a>
+          <Link className="navbar-brand" to="/">E-Shop</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mb-2 mb-lg-0 links">
             <li className="nav-item">
-                <a className="btn btn-primary" href="/cart">
-                  Cart &nbsp;&nbsp;
+              <Link className="btn btn-primary" to="/cart">
+              Cart &nbsp;&nbsp;
                   {
                     cartItems.length>0 && 
                     <span className="badge text-bg-warning">{cartItems.length}</span>
                   }
-                  </a>
+              </Link>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="/register">Register</a>
--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,65 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
+import AppContext from "../../context";
 
 import Header from "../../components/Shared/Header/Header";
 import CartItem from "../../components/Cart/CartItem/CartItem";
 import "./Cart.css";
 
 function Cart() {
-  const [cartItems, setCartItem] = useState([]);
-  const [totalPrice, setTotalPrice] = useState(0);
-
-  useEffect(() => {
-
-    const products = localStorage.getItem("cartItems");
-    if(products){
-      const items = JSON.parse(products);
-      updateTotalPrice(items);
-        setCartItem(items);
-    }
-  }, []);
-
-  // Step 3: takes new quantity and update cart items.
-  function updatePrice(item, newQuantity) {
-  
-    let items = cartItems;
-    let cartItemIndex = items.findIndex((i) => i.title === item.title);
-    items[cartItemIndex].qty = newQuantity;
-    setCartItem(items);
-    updateTotalPrice(items);
-  }
-
-  // 1. Update Total Price = sum of price*quantity for each cart items.
-  function updateTotalPrice(res) {
-    // console.log(res);
-    let sum = 0;
-    for (let i = 0; i < res.length; i++) {
-      sum = sum + Number(res[i].price) * Number(res[i].qty);
-    }
-    // console.log(sum);
-    setTotalPrice(Math.ceil(sum));
-  }
-
-  function deleteItem(index) {
-    let items = cartItems;
-    items.splice(index, 1);
-    setCartItem(items);
-    updateTotalPrice(items);
-    // update items in localStorage.
-    localStorage.setItem("cartItems", JSON.stringify(items));
-  }
+  const {cartItems} = useContext(AppContext);
+  console.log(cartItems);
 
   return (
     <div className="cart-container">
       <Header />
       {/* 2. Print updated total price */}
-      <h4 className="totalPrice">Total Price: {totalPrice}</h4>
+      {/* <h4 className="totalPrice">Total Price: {totalPrice}</h4> */}
       <div>
         {cartItems.map((item, index) => (
           <CartItem
             // 4. Pass function as props.
-            updatePrice={updatePrice}
+            
             key={index}
-            delete={deleteItem}
+            
             item={item}
             index={index}
           />
